092688639e2ed9564b0eab1c3ac466c0a6f2f39d
Vincent <67933778+cokesnail@users.noreply.github.com>
2022-11-08 23:54:12 +0900

    [level 0] Title: 자릿수 더하기, Time: 0.23 ms, Memory: 31.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120906. 자릿수 더하기/자릿수 더하기.cs
@@ -0,0 +1,23 @@
+using System;
+
+public class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        
+        // 12를 10으로 나누고 몫과 나머지를 더한다.
+        
+        // 103을 10으로 나누고 몫은 10 나머지 3
+        // 10을 10으로 나누고 몫은 1 나머지 0
+        // 몫은 10보다 작으면 그만.
+        // 그래서 마지막 몫과 나머지를 다 더한다.
+        
+        while (n > 10) {
+            answer += n % 10;
+            n = n / 10;
+        }
+        
+        answer += n;
+        
+        return answer;
+    }
+}
